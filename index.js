1bf7aa1afb6e82b64ea471e5e0f03697966439a9
Thiago Lagden <lagden@gmail.com>
2018-12-12 18:17:35 -0200

    colocando tudo em um método

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 'use strict'
 
 /**
- * Helper para criação das chaves: valores
- * @private
- *
- * @param {object} child        - objeto que será achatado
- * @param {string} separator    - caractere separador das chaves
- * @param {array} [_path=[]]    - armazena as chaves
- * @returns {array} Retorna um coleção de objetos
- */
-function _(child, separator, _path = []) {
-	return [].concat(
-		...Object.keys(child).map(key => {
-			if (typeof child[key] === 'object' && child[key] !== null && child[key] !== undefined) {
-				return _(child[key], separator, [..._path, ...[key]])
-			}
-			return {[[..._path, ...[key]].join(separator)]: child[key]}
-		})
-	)
-}
-
-/**
- * Achata o objeto
+ * Achata o `objeto` em um único nível.
  *
  * @param {object} object        - objeto que será achatado
- * @param {string} separator     - caractere separador das chaves
- * @returns {object} Retorna objeto achatado
+ * @param {string} separator     - símbolo usado para unir as chaves
+ * @returns {object} Retorna um novo objeto achatado
  */
-function flattenDeepObject(object, separator = '.') {
-	return Object.assign({}, ..._(object, separator))
+function flattenObject(object, separator = '.') {
+	return Object.assign({}, ...(function _recursive(child, _path = []) {
+		return [].concat(
+			...Object.keys(child).map(key => {
+				if (typeof child[key] === 'object' && child[key] !== null && child[key] !== undefined) {
+					return _recursive(child[key], [..._path, ...[key]])
+				}
+				return {[[..._path, ...[key]].join(separator)]: child[key]}
+			})
+		)
+	})(object))
 }
 
-module.exports = flattenDeepObject
+module.exports = flattenObject
